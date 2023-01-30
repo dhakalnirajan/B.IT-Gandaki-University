c502fff2dcf6a2a2d73e132e0c6b90a8b9debe53
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-01-30 21:38:40 +0545

    Deleted folder to renew folders

--- First Year/First Semester/Programming With C/Lab work/Print age of a user.c
+++
@@ -1,8 +0,0 @@
-// Print age of a user
-#include <stdio.h>
-int main() {
-	int age;
-	printf("Please enter your age \n");
-	scanf("%d", age);
-	return 0;
-}
--- First Year/First Semester/Programming With C/Lab work/hello world.c
+++
@@ -1,6 +0,0 @@
-// Hello World
-#include <stdio.h>
-int main() {
-	printf("Hello World");
-	return 0;
-}
