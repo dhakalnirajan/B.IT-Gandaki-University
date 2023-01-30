c2b91efe6a8ab9008ad48e387f23a1854363ca1f
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-01-30 22:37:04 +0545

    Deleted Folder: Lab1

--- First Year/First Semester/Programming With C/Lab 1/02. Print Sum of 2 and 3.c
+++
@@ -1,8 +0,0 @@
-// Sum of 2 and 3
-#include <stdio.h>
-int main() {
-	int a = 2, b = 3, sum;
-	sum = a + b;
-	printf("The sum is %d", sum);
-	return 0;
-}
--- First Year/First Semester/Programming With C/Lab 1/03. Input Age of a user and Print It.c
+++
@@ -1,9 +0,0 @@
-// WAP to input and print age of a user
-#include <stdio.h>
-int main() {
-	int age;
-	printf("Please enter your age \n");
-	scanf("%d", &age);
-	printf("The age is: %d", age);
-	return 0;
-}
