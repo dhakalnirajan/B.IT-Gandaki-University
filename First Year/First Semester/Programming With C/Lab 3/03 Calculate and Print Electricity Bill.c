042a097cabc7e3258e49a8a4eb3bc59477cfd44c
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-03-13 18:15:17 +0545

    Update 03 Calculate and Print Electricity Bill.c

--- First Year/First Semester/Programming With C/Lab 3/03 Calculate and Print Electricity Bill.c
+++ First Year/First Semester/Programming With C/Lab 3/03 Calculate and Print Electricity Bill.c
@@ -1,42 +1,51 @@
 // WAP to calculate and print the electricity bill of a given customer.
 
-#include<stdio.h>
-int main(){
-	float unit,cost;
-	int cid;
-	char name[50];
-	printf("Enter customer id: ");
-	scanf("%d",&cid);
-	printf("Enter name:");
-	scanf("%s",&name);
-	printf("Enter unit: ");
-	scanf("%f",&unit);
-	if(unit <= 199){
-		cost=unit*1.20;
+#include <stdio.h>
+
+int main() {
+    int customer_id, unit;
+    char name[50];
+
+    float charge_per_unit, total_charge, surcharge = 0, net_amount;
+
+    printf("Enter customer id: ");
+    scanf("%d", &customer_id);
+
+    printf("Enter customer name: ");
+    scanf("%s", name);
+
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &unit);
+
+    if (unit <= 199) {
+        charge_per_unit = 1.20;
     }
-	else if(unit == 200 && unit <400){
-        cost=unit*1.50;
-        printf("%f", cost);
+    else if (unit >= 200 && unit < 400) {
+        charge_per_unit = 1.50;
     }
-	else if(unit == 400 && unit <600){
-        cost=unit*1.80;
-        printf("%f", cost);
-    }	
-	else if(unit >= 600){
-        cost=unit*2.0;
-        printf("%f", cost);
+    else if (unit >= 400 && unit < 600) {
+        charge_per_unit = 1.80;
     }
-	else{
-		cost=100;
-        printf("%f", cost);
+    else {
+        charge_per_unit = 2.0;
     }
-	if(cost > 400){
-		cost = cost + ((15/100)*cost);
-        printf("%f", cost);
-	}
-    else if(cost < 100){
-		cost=100;
-	    printf("Dear %s you need to pay %f",name,cost);
+
+    total_charge = unit * charge_per_unit;
+
+    if (total_charge > 400) {
+        surcharge = total_charge * 0.15;
     }
+
+    net_amount = total_charge + surcharge;
+
+    if (net_amount < 100) {
+        net_amount = 100;
+    }
+
+    printf("\nCustomer ID: %d", customer_id);
+    printf("\nCustomer Name: %s", name);
+    printf("\nNumber of units consumed: %d", unit);
+    printf("\nNet Amount Paid By the Customer: Rs. %.2f", net_amount);
+
     return 0;
 }
