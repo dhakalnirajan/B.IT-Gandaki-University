a5f7e30f1fee4ebbf31bf772f8d85091a835f008
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-03-25 18:15:09 +0545

    Fixed Error in Ascending Order using array program.c

--- First Year/First Semester/Programming With C/Lab 5/03 ascending order using array.c
+++ First Year/First Semester/Programming With C/Lab 5/03 ascending order using array.c
@@ -2,20 +2,18 @@
 
 int main()
 {
-    int n, i, j, tmp;
-    printf("Input the size of array : ");
-    scanf("%d", &n);
-    int arr1[n];
-    printf("Input %d elements in the array :\n",n);
-    for(i=0;i<n;i++)
+    int arr1[10], i, j, tmp;
+
+    printf("Enter 10 elements in the array:\n");
+    for(i=0;i<10;i++)
     {
-        printf("element - %d : ",i);
+        printf("Enter element %d: ", i+1);
         scanf("%d",&arr1[i]);
     }
-    
-    for(i=0; i<n; i++)
+
+    for(i=0; i<10; i++)
     {
-        for(j=i+1; j<n; j++)
+        for(j=i+1; j<10; j++)
         {
             if(arr1[j] < arr1[i])
             {
@@ -25,10 +23,58 @@ int main()
             }
         }
     }
+
     printf("\nElements of array in sorted ascending order:\n");
-    for(i=0; i<n; i++)
+    for(i=0; i<10; i++)
+    {
+        printf("%d ", arr1[i]);
+    }
+
+    return 0;
+}
+
+
+// To Dynamically ask user the size of array and print the numbers in Ascending order, following is the program. To execute the program, uncomment the code:
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, tmp;
+    printf("Input the size of array : ");
+    scanf("%d", &n);
+    int arr1[n];
+    printf("Input %d elements in the array :\n", n);
+
+    // Taking input from user
+    for(i = 0; i < n; i++)
+    {
+        printf("Element - %d : ", i);
+        scanf("%d", &arr1[i]);
+    }
+
+    // Sorting array elements in ascending order
+    for(i = 0; i < n; i++)
+    {
+        for(j = i+1; j < n; j++)
+        {
+            if(arr1[j] < arr1[i])
+            {
+                tmp = arr1[i];
+                arr1[i] = arr1[j];
+                arr1[j] = tmp;
+            }
+        }
+    }
+
+    // Printing the sorted array
+    printf("\nElements of array in sorted ascending order:\n");
+    for(i = 0; i < n; i++)
     {
         printf("%d  ", arr1[i]);
     }
+
     return 0;
 }
+*/
