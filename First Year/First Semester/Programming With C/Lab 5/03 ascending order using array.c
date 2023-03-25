e6b93c811a79334887b3a966348e7affe57cfe20
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-03-25 15:53:50 +0545

    Fixed error for ascending order in array.c

--- First Year/First Semester/Programming With C/Lab 5/03 ascending order using array.c
+++ First Year/First Semester/Programming With C/Lab 5/03 ascending order using array.c
@@ -5,19 +5,19 @@ int main()
     int n, i, j, tmp;
     printf("Input the size of array : ");
     scanf("%d", &n);
-	int arr1[n];
-   	printf("Input %d elements in the array :\n",n);
-   	for(i=0;i<n;i++)
+    int arr1[n];
+    printf("Input %d elements in the array :\n",n);
+    for(i=0;i<n;i++)
     {
-		printf("element - %d : ",i);
-		scanf("%d",&arr1[i]);
+        printf("element - %d : ",i);
+        scanf("%d",&arr1[i]);
     }
-	
+    
     for(i=0; i<n; i++)
     {
         for(j=i+1; j<n; j++)
         {
-            if(arr1[j] <arr1[i])
+            if(arr1[j] < arr1[i])
             {
                 tmp = arr1[i];
                 arr1[i] = arr1[j];
@@ -25,9 +25,10 @@ int main()
             }
         }
     }
-    printf("\nElements of array in sorted descending order:\n");
-    for(i=n-1; i>=0; i--)
+    printf("\nElements of array in sorted ascending order:\n");
+    for(i=0; i<n; i++)
     {
         printf("%d  ", arr1[i]);
     }
+    return 0;
 }
