7e7fedde99713bdc43d5cd0b769f378c4b7529c2
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-06-26 18:25:58 +0545

    Create AreaOfCircle.java

---
+++ First Year/Second Semester/Lab/Java/Lab 1/AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfCircle {
+
+    public static final double PI = 3.1415;
+
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = PI * radius * radius;
+        System.out.println("Area of circle with radius " + radius + " is " + area);
+    }
+}
