2cdd2a2d2e4c31880c437ce11384e0e0ff0c445f
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2024-05-30 15:58:32 +0545

    Update FCFS CPU Scheduling Algorithm.c

--- Second Year/Fourth Semester/Operating System Lab/FCFS CPU Scheduling Algorithm.c
+++ Second Year/Fourth Semester/Operating System Lab/FCFS CPU Scheduling Algorithm.c
@@ -1,30 +1,29 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void main(){
-    int bt[20], wt[20], tat[20], n, i;
-    float wt_avg, tat_avg;
-    clrscr();
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-    for(i=0; i<n; i++){
-        printf("Enter the burst time of process %d: ", i);
-        scanf("%d", &bt[i]);
+int main(){
+    int bt[20], wt[20], tat[20], i ,n;
+    float wtavg, tatavg;
+    //clrscr();
+    printf("\n Enter the number of processes --");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("\nEnter Brust time for Process %d --",i);
+        scanf("%d",&bt[i]);
     }
-    wt[0] = wt_avg = 0;
-    tat[0] = tat_avg = bt[0];
-    for(i=1; i<n; i++){
-        wt[i] = wt[i-1] + bt[i-1];
+
+    wt[0] = wtavg = 0;
+    tat[0] = tatavg = bt[0];
+    for(i=1;i<n;i++){
+        wt[i] = wt[i-1]+ bt[i-1];
         tat[i] = tat[i-1] + bt[i];
-        wt_avg += wt[i];
-        tat_avg += tat[i];
-    }
-    printf("\t Process \t Burst Time \t Waiting Time \t Turnaround Time \n");
-    
-    for(i = 0; i < n; i++){
-    	printf("\n\tP%d \t\t %d \t\t %d \t\t%d", i bt[i], wt[i], tat[i]);
-    	printf("\nAverage Waiting Time:  %f", wt_avg);
-    	printf("\nAverage Turnaround Time: %f", ta_avg);
-    	printf("\n");
-    	getch();
-	}
+        wtavg = wtavg + wt[i];
+        tatavg = tatavg + tat[i];
+      }
+      printf("\tPROCESS\tBURST TIME\t WAITING TIME\t TURN AROUND TIME\n");
+      for(i=0;i<n;i++){
+        printf("\n\tP%d\t\t%d\t\t%d\t\t%d", i, bt[i], wt[i], tat[i]);
+        printf("\nAverage Waiting Time -- %f", wtavg);
+        printf("\nAverage Turnaround Time -- %f", tatavg);
+        //getch();
+      }
 }
