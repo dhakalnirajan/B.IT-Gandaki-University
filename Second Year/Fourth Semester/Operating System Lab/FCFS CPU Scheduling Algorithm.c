144ac29e49205e763cd2ebb4f0d833d5678e5883
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2024-05-30 15:53:49 +0545

    Create FCFS CPU Scheduling Algorithm.c

---
+++ Second Year/Fourth Semester/Operating System Lab/FCFS CPU Scheduling Algorithm.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main(){
+    int bt[20], wt[20], tat[20], n, i;
+    float wt_avg, tat_avg;
+    clrscr();
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    for(i=0; i<n; i++){
+        printf("Enter the burst time of process %d: ", i);
+        scanf("%d", &bt[i]);
+    }
+    wt[0] = wt_avg = 0;
+    tat[0] = tat_avg = bt[0];
+    for(i=1; i<n; i++){
+        wt[i] = wt[i-1] + bt[i-1];
+        tat[i] = tat[i-1] + bt[i];
+        wt_avg += wt[i];
+        tat_avg += tat[i];
+    }
+    printf("\t Process \t Burst Time \t Waiting Time \t Turnaround Time \n");
+    
+    for(i = 0; i < n; i++){
+    	printf("\n\tP%d \t\t %d \t\t %d \t\t%d", i bt[i], wt[i], tat[i]);
+    	printf("\nAverage Waiting Time:  %f", wt_avg);
+    	printf("\nAverage Turnaround Time: %f", ta_avg);
+    	printf("\n");
+    	getch();
+	}
+}
