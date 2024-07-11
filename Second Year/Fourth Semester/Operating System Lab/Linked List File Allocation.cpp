fec2f6df7d3a82bde2cd62b42cbc592169b4a2de
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2024-07-11 16:24:22 +0545

    Create Linked List File Allocation.cpp

---
+++ Second Year/Fourth Semester/Operating System Lab/Linked List File Allocation.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct block {
+    int bno;
+    struct block *next;
+};
+
+struct fileTable {
+    char name[20];
+    int nob;
+    struct block *sb;
+} ft[30];
+
+// Function to free the linked list of blocks
+void freeBlocks(struct block *head) {
+    struct block *current = head;
+    while (current != NULL) {
+        struct block *temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    int n, i, j;
+    char s[20];
+    struct block *temp;
+
+    printf("Enter the number of files: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("\nEnter the file name %d: ", i + 1);
+        scanf("%19s", ft[i].name);  // Limit input size to prevent buffer overflow
+        printf("\nEnter the number of blocks for file %d: ", i + 1);
+        scanf("%d", &ft[i].nob);
+        ft[i].sb = NULL;
+
+        for (j = 0; j < ft[i].nob; j++) {
+            temp = (struct block *)malloc(sizeof(struct block));
+            if (temp == NULL) {
+                fprintf(stderr, "Memory allocation failed.\n");
+                exit(1);
+            }
+            printf("\nEnter the block number %d for file %d: ", j + 1, i + 1);
+            scanf("%d", &temp->bno);
+            temp->next = ft[i].sb;
+            ft[i].sb = temp;
+        }
+    }
+
+    printf("\nEnter the file name to be searched: ");
+    scanf("%19s", s);  // Limit input size to prevent buffer overflow
+
+    for (i = 0; i < n; i++) {
+        if (strcmp(s, ft[i].name) == 0) {
+            printf("\nFILE NAME\tNUMBER OF BLOCKS\tBLOCKS OCCUPIED\n");
+            printf("%s\t\t%d\t\t", ft[i].name, ft[i].nob);
+            temp = ft[i].sb;
+            while (temp != NULL) {
+                printf("%d ", temp->bno);
+                temp = temp->next;
+            }
+            printf("\n");
+            break;
+        }
+    }
+
+    if (i == n) {
+        printf("\nFile not found.\n");
+    }
+
+    // Free allocated memory for blocks in each file
+    for (i = 0; i < n; i++) {
+        freeBlocks(ft[i].sb);
+    }
+
+    return 0;
+}
