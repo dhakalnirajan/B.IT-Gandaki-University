2505a9d140266eb6c5569185cc8324a3d2a4c0bd
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2025-05-08 15:48:46 +0545

    Update caeser-cipher.c

--- Third Year/Sixth Semester/Digital Forensics/caeser-cipher.c
+++ Third Year/Sixth Semester/Digital Forensics/caeser-cipher.c
@@ -2,16 +2,42 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
     int i, x;
     char str[100];
-    printf("Enter a message \n");
-    gets(str);
     int key = 3;
 
-    begin:
-
-    printf("\n Please choose the following options:\n");
+begin:
+    printf("\nPlease choose following options:\n");
     printf("1 = Encrypt the string.\n");
-    printf("")
+    printf("2 = Decrypt the string.\n");
+    printf("3 = Exit\n");
+    scanf("%d", &x);
+
+    switch (x) {
+        case 1:
+            printf("Enter a message\n");
+            gets(str);
+            for (i = 0; i < 100 && str[i] != '\0'; i++) {
+                str[i] = str[i] + key; //the key for encryption is added to ASCII value
+            }
+            printf("\nEncrypted string: %s\n", str);
+            goto begin;
+
+        case 2:
+            printf("Enter a message\n");
+            gets(str);
+            for (i = 0; i < 100 && str[i] != '\0'; i++) {
+                str[i] = str[i] - key; //the key for encryption is subtracted to ASCII value
+            }
+            printf("\nDecrypted string: %s\n", str);
+            goto begin;
+
+        case 3:
+            exit(0);
+
+        default:
+            printf("\nError\n");
+            return 0;
+    }
 }
