0388196ccaee7bf0d72f3d84ac5a109ae1669d9e
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2025-05-08 15:45:07 +0545

    Digital Forensics in Sixth Semester Uploaded

---
+++ Third Year/Sixth Semester/Digital Forensics/ceaser-cipher.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+    int i, x;
+    char str[100];
+    printf("Enter a message \n");
+    gets(str);
+    int key = 3;
+
+    begin:
+
+    printf("\n Please choose the following options:\n");
+    printf("1 = Encrypt the string.\n");
+    printf("")
+}
